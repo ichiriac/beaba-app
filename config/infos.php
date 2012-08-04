beac0fcea703ca07b82dcfb8acd049c2356b9500
Ioan CHIRIAC <i.chiriac@yahoo.com>
2012-08-04 05:36:15 +0200

    add some samples

---
+++ config/infos.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'application'   => array(
+        'title'         => 'Default Application',
+        'description'   => 'An empty application skeleton',
+        'author'        => 'Ioan CHIRIAC',
+        'website'       => 'http://ichiriac.github.com/beaba'
+    )
+);
